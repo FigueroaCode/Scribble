c08aef087212b0f4cc2171f13da48fe717317e66
Miranda Ferguson <Miranda-Ferguson@Mirandas-MacBook-Pro.local>
2017-04-01 19:08:06 -0400

    creating course.h

--- course.h
+++ course.h
@@ -1,11 +1,26 @@
 #ifndef COURSE_H
 #define COURSE_H
 
+#include <QString>
+
 
 class Course
 {
+private:
+    QString instructorName;
+    QString courseID;
+    QString courseName;
+    QString institution;
 public:
     Course();
+    QString getInstructorName();
+    QString getCourseID();
+    QString getCourseName();
+    QString getInstitution();
+    void setInstructorName(QString instructor);
+    void setCourseID(QString id);
+    void setCourseName(QString name);
+    void setInstitution(QString school);
 };
 
 #endif // COURSE_H
