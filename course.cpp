3502e5009e762356a4ec0681e4c64556fc749c02
Miranda Ferguson <Miranda-Ferguson@Mirandas-MacBook-Pro.local>
2017-04-01 19:07:18 -0400

    defining some functions

--- course.cpp
+++ course.cpp
@@ -1,6 +1,43 @@
 #include "course.h"
 
+#include <QString>
+
 Course::Course()
 {
+    instructorName = "";
+    courseID = "";
+    courseName = "";
+    institution = "";
+}
 
+QString Course::getInstructorName(){
+    return instructorName;
+}
+
+QString Course::getCourseID(){
+    return courseID;
+}
+
+QString Course::getCourseName(){
+    return courseName;
+}
+
+QString Course::getInstitution(){
+    return institution;
+}
+
+void Course::setInstructorName(QString instructor){
+    instructorName = instructor;
+}
+
+void Course::setCourseID(QString id){
+    courseID = id;
+}
+
+void Course::setCourseName(QString name){
+    courseName = name;
+}
+
+void Course::setInstitution(QString school){
+    institution = school;
 }
